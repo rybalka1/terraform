7c6a1175e77c219eb660dcaa1eb88bbcce763698
Дмитрий Рыбалка <rybalka.dmitrii@gmail.com>
2022-01-20 15:37:44 +0300

    Made an extra task

--- main.tf
+++ main.tf
@@ -7,13 +7,6 @@ terraform {
   }
 }
 
-provider "yandex" {
-  token     = var.ya_token
-  cloud_id  = "b1g3jddf4nv5e9okle7p"
-  folder_id = "b1g10hb6v5a9qe3ut7na"
-  zone      = "ru-central1-a"
-}
-
 resource "yandex_compute_instance" "vm-1" {
   name = "chapter5-lesson2-rybalka-dmitrii"
 
---
+++ provider.tf
@@ -0,0 +1,6 @@
+provider "yandex" {
+  token     = var.ya_token
+  cloud_id  = "b1g3jddf4nv5e9okle7p"
+  folder_id = "b1g10hb6v5a9qe3ut7na"
+  zone      = "ru-central1-a"
+}
